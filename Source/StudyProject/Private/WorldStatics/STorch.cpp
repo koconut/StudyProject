1c598b08728d27dea7014d0def31780b1e0224c9
DODAO <yjsuk0818@naver.com>
2023-10-29 22:20:30 +0900

    feat: Ch03

---
+++ Source/StudyProject/Private/WorldStatics/STorch.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldStatics/STorch.h"
+#include "Components/BoxComponent.h"
+#include "Components/PointLightComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "GameFramework/RotatingMovementComponent.h"
+
+// Sets default values
+ASTorch::ASTorch()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+    SetRootComponent(BoxComponent);
+
+    BodyStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyStaticMeshComponent"));
+    BodyStaticMeshComponent->SetupAttachment(GetRootComponent());
+    BodyStaticMeshComponent->SetRelativeLocation(FVector(0.f, 0.f, -30.f));
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> BodyStaticMesh(TEXT("/Script/Engine.StaticMesh'/Game/StarterContent/Architecture/Pillar_50x500.Pillar_50x500'"));
+    // 경로 정보가 플레이 중에는 변경되지 않으므로, static 키워드 사용.
+    if (true == BodyStaticMesh.Succeeded())
+    {
+        BodyStaticMeshComponent->SetStaticMesh(BodyStaticMesh.Object);
+    }
+
+    PointLightComponent = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLightComponent"));
+    PointLightComponent->SetupAttachment(GetRootComponent());
+    PointLightComponent->SetRelativeLocation(FVector(0.f, 0.f, 500.f));
+
+    ParticleSystemComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleSystemComponent"));
+    ParticleSystemComponent->SetupAttachment(GetRootComponent());
+    ParticleSystemComponent->SetRelativeLocation(FVector(0.f, 0.f, 500.f));
+    static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleTemplate(TEXT("/Script/Engine.ParticleSystem'/Game/StarterContent/Particles/P_Fire.P_Fire'"));
+    if (true == ParticleTemplate.Succeeded())
+    {
+        ParticleSystemComponent->SetTemplate(ParticleTemplate.Object);
+    }
+
+    RotatingMovementComponent = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("RotatingMovementComponent"));
+}
+
+void ASTorch::BeginPlay()
+{
+    Super::PostInitializeComponents();
+
+    RotationSpeed = 300.f;
+    RotatingMovementComponent->RotationRate = FRotator(0.f, RotationSpeed, 0.f);
+}
+
+void ASTorch::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    // AddActorWorldRotation(FRotator(0.f, RotationSpeed * DeltaSeconds, 0.f));
+}
